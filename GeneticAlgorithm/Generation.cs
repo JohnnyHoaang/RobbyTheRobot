1f54a798d00a917c3f8dc62a3e9d7b13563a9630
vinhdath <vinhdat06@hotmail.com>
2022-10-28 10:45:00 -0400

    fix indexes

--- GeneticAlgorithm/Generation.cs
+++ GeneticAlgorithm/Generation.cs
@@ -1,13 +1,13 @@
 namespace GeneticAlgorithm
 {
   using System;
-  internal class Generation : IGeneration
+  public class Generation : IGeneration
   {
 
     public delegate double FitnessEventHandler(IChromosome chromosome, IGeneration generation);
 
     private IGeneticAlgorithm _geneticAlgorithm;
-    private FitnessEventHandler _fitnerssEventHandler;
+    private FitnessEventHandler _fitnessEventHandler;
     private int? _seed;
     private IChromosome[] _chromosomes;
 
@@ -15,45 +15,56 @@ namespace GeneticAlgorithm
     // <summary>
     /// The average fitness across all Chromosomes
     /// </summary>
-    public double AverageFitness { get{
-     
-       double totalFitness = 0;
-       foreach(var chromosome in _chromosomes){
-        totalFitness += chromosome.Fitness;
-       }
+    public double AverageFitness
+    {
+      get
+      {
 
-       return totalFitness / _chromosomes.Length;
-      
-    } }
+        double totalFitness = 0;
+        foreach (var chromosome in _chromosomes)
+        {
+          totalFitness += chromosome.Fitness;
+        }
+
+        return totalFitness / _chromosomes.Length;
+
+      }
+    }
 
     /// <summary>
     /// The maximum fitness across all Chromosomes
     /// </summary>
-    public double MaxFitness { get{
-       int highest = 0;
-       for(int i = 1; i < _chromosomes.Length; i++){
-        if(_chromosomes[highest].Fitness < _chromosomes[i].Fitness){
-          highest = i;
+    public double MaxFitness
+    {
+      get
+      {
+        int highest = 0;
+        for (int i = 1; i < _chromosomes.Length; i++)
+        {
+          if (_chromosomes[highest].Fitness < _chromosomes[i].Fitness)
+          {
+            highest = i;
+          }
         }
-       }
-       return _chromosomes[highest].Fitness;
-    } }
+        return _chromosomes[highest].Fitness;
+      }
+    }
 
     /// <summary>
     /// Returns the number of Chromosomes in the generation
     /// </summary>
-    public long NumberOfChromosomes { get =>  _chromosomes.Length; }
+    public long NumberOfChromosomes { get => _chromosomes.Length; }
 
-   
 
-    public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnerssEventHandler, int? seed)
+
+    public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessEventHandler, int? seed)
     {
-      if (geneticAlgorithm == null || fitnerssEventHandler == null)
+      if (geneticAlgorithm == null || fitnessEventHandler == null)
       {
         throw new NullReferenceException("null object");
       }
       _geneticAlgorithm = geneticAlgorithm;
-      _fitnerssEventHandler = fitnerssEventHandler;
+      _fitnessEventHandler = fitnessEventHandler;
       _seed = seed;
     }
 
@@ -64,7 +75,7 @@ namespace GeneticAlgorithm
       {
         throw new NullReferenceException("null object");
       }
-      
+
       // initialize array chromosomes
       _chromosomes = new IChromosome[chromosome.Length];
       for (int i = 0; i < chromosome.Length; i++)
@@ -79,32 +90,51 @@ namespace GeneticAlgorithm
     /// Retrieves the IChromosome from the generation
     /// </summary>
     /// <value>The selected IChromosome</value>
-    IChromosome IGeneration.this[int index]
+    public IChromosome this[int index]
     {
       get
       {
-          //TODO: validate index
-          return _chromosomes[index];
+        //TODO: validate index
+        return _chromosomes[index];
 
       }
     }
 
-  }
-  // TODO :
-  internal interface IGenerationDetails : IGeneration
-  {
-    /// <summary>
-    /// Randomly selects a parent by comparing its fitness to others in the population
-    /// </summary>
-    /// <returns></returns>
-    IChromosome SelectParent();
 
-    /// <summary>
-    /// Computes the fitness of all the Chromosomes in the generation. 
-    /// Note, a FitnessEventHandler deleagte is invoked for every fitness function that must be calculated and is provided by the user
-    /// Note, if NumberOfTrials is greater than 1 in IGeneticAlgorithm, 
-    /// the average of the number of trials is used to compute the final fitness of the Chromosome.
-    /// </summary>
-    void EvaluateFitnessOfPopulation();
+    // TODO :
+
+    internal class GenerationDetails : IGenerationDetails
+    {
+      public double AverageFitness => throw new NotImplementedException();
+
+      public double MaxFitness => throw new NotImplementedException();
+
+      public long NumberOfChromosomes => throw new NotImplementedException();
+
+      public IChromosome this[int index] => throw new NotImplementedException();
+
+      /// <summary>
+      /// Randomly selects a parent by comparing its fitness to others in the population
+      /// </summary>
+      /// <returns></returns>
+      IChromosome IGenerationDetails.SelectParent()
+      {
+        throw new NotImplementedException();
+      }
+
+      /// <summary>
+      /// Computes the fitness of all the Chromosomes in the generation. 
+      /// Note, a FitnessEventHandler deleagte is invoked for every fitness function that must be calculated and is provided by the user
+      /// Note, if NumberOfTrials is greater than 1 in IGeneticAlgorithm, 
+      /// the average of the number of trials is used to compute the final fitness of the Chromosome.
+      /// </summary>
+      void IGenerationDetails.EvaluateFitnessOfPopulation()
+      {
+        throw new NotImplementedException();
+      }
+
+
+    }
+
   }
 }
--- GeneticAlgorithmTest/UnitTest1.cs
+++ GeneticAlgorithmTest/UnitTest1.cs
@@ -1,13 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System;
+using GeneticAlgorithm;
 namespace GeneticAlgorithmTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-        {
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestGenerationConstructor()
+        {   
+         
+            Generation generation = new Generation(null, null, 0);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestGenerationChromosomes()
+        {   
+                     
+            Generation generation = new Generation(null);
+         
+        }
+
     }
 }
