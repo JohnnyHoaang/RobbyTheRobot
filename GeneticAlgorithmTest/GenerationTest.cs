48336315d5c9dff94bbf6125fd877e7597613659
vinhdath <vinhdat06@hotmail.com>
2022-11-04 11:55:18 -0400

    update test for generation

--- GeneticAlgorithmTest/GenerationTest.cs
+++ GeneticAlgorithmTest/GenerationTest.cs
@@ -6,41 +6,80 @@ using System;
 
 namespace GeneticAlgorithmTest
 {
-    [TestClass]
-    public class GenerationTest
+  [TestClass]
+  public class GenerationTest
+  {
+    [TestMethod]
+    public void NumberOfChromosomesChromosomesTest()
     {
-      [TestMethod]
-        public void NumberOfChromosomesChromosomesTest()
-        {
-          int[] arr = {1,2,3};
-          
-          IChromosome chromosome = new Chromosome(arr, 3);
-          IChromosome[] chrome = {chromosome};
-          IGeneration generation = new Generation(chrome); 
-          Assert.AreEqual(1,generation.NumberOfChromosomes);
-        }
+      int[] arr = { 9, 11, 5, 7, 20, 19, 12, 3, 4, 7 };
+      IChromosome[] list = new Chromosome[10];
 
-        [TestMethod]
-        public void AverageChromosomesTest()
-        {
-          int[] arr = {1,2,3};
-          
-          IChromosome chromosome = new Chromosome(arr, 3);
-          IChromosome[] chrome = {chromosome};
-          
-          IGeneration generation = new Generation(chrome); 
-          Assert.AreEqual(0,generation.AverageFitness);
-        }
+      for (int i = 0; i < list.Length; i++)
+      {
+        Chromosome chromo = new Chromosome(null, 10, null);
+        chromo.Fitness = arr[i];
+        list[i] = chromo;
+      }
 
-          [TestMethod]
-        public void MaxFitnessChromosomesTest()
-        {
-          int[] arr = {1,2,3};
-          
-          IChromosome chromosome = new Chromosome(arr, 3);
-          IChromosome[] chrome = {chromosome};
-          IGeneration generation = new Generation(chrome); 
-          Assert.AreEqual(0,generation.MaxFitness);
-        }
+      IGeneration generation = new GenerationDetails(list);
+      Assert.AreEqual(10, generation.NumberOfChromosomes);
     }
+
+    [TestMethod]
+    public void AverageChromosomesTest()
+    {
+      int[] arr = { 9, 11, 5, 7, 20, 19, 12, 3, 4, 7 };
+      IChromosome[] list = new Chromosome[10];
+
+      for (int i = 0; i < list.Length; i++)
+      {
+        Chromosome chromo = new Chromosome(null, 10, null);
+        chromo.Fitness = arr[i];
+        list[i] = chromo;
+      }
+
+      IGeneration generation = new GenerationDetails(list);
+      Assert.AreEqual(9.7, generation.AverageFitness);
+    }
+
+    [TestMethod]
+    public void MaxFitnessChromosomesTest()
+    {
+
+      int[] arr = { 9, 11, 5, 7, 20, 19, 12, 3, 4, 7 };
+      IChromosome[] list = new Chromosome[10];
+
+      for (int i = 0; i < list.Length; i++)
+      {
+        Chromosome chromo = new Chromosome(null, 10, null);
+        chromo.Fitness = arr[i];
+        list[i] = chromo;
+      }
+
+      IGeneration generation = new GenerationDetails(list);
+      Assert.AreEqual(20, generation.MaxFitness);
+    }
+
+    [TestMethod]
+    public void SelectParentTest()
+    {
+      int[] arr = { 9, 11, 5, 7, 20, 19, 12, 3, 4, 7 };
+      IChromosome[] list = new Chromosome[10];
+
+      for (int i = 0; i < list.Length; i++)
+      {
+        Chromosome chromo = new Chromosome(null, 10, null);
+        chromo.Fitness = arr[i];
+        list[i] = chromo;
+      }
+
+      IGenerationDetails generation = new GenerationDetails(list);
+      // get the chromosome with highest fitness
+      IChromosome highestChromo = generation.SelectParent();
+      // use fitness value of retrun chromosome to check position in array
+      Assert.AreEqual(20, highestChromo.Fitness);
+
+    }
+  }
 }
