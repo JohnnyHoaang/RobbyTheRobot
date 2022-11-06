4b44953d23293acb1a10becbc6564832937915b7
vinhdath <vinhdat06@hotmail.com>
2022-11-05 20:05:00 -0400

    remove dead code from comments

--- GeneticAlgorithmTest/UnitTest1.cs
+++ GeneticAlgorithmTest/UnitTest1.cs
@@ -19,11 +19,7 @@ namespace GeneticAlgorithmTest
             List<int> points = new List<int>();
             points.Add(2);
             points.Add(4);
-            // IChromosome[] somes = chromosome.CrossChromosomes(chromosome2, points);
-            // Assert.AreEqual(2, somes[0][1]);
-            // Assert.AreEqual(7, somes[0][2]);
-            // Assert.AreEqual(6, somes[0][3]);
-            // Assert.AreEqual(5, somes[0][4]);
+      
         }
         [TestMethod]
         public void MutationTest()
@@ -33,11 +29,7 @@ namespace GeneticAlgorithmTest
             int [] genes2 = new int[]{9,8,7,6,5,4,3};
             Chromosome chromosome2 = new Chromosome(genes2,genes.Length);
             IChromosome[] chromosomes = {chromosome, chromosome2};
-            // chromosome.Mutate(chromosomes,0.01);
-            // foreach(int gene in chromosomes[0].Genes)
-            // {
-            //     Console.Write(gene + ", ");
-            // }
+          
         }
 
      
